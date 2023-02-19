653c2b174f32c335323dd2fa08359bda590736c5
Kiruba0397 <kirubawincelinc@stgit.com>
2023-02-19 20:53:01 +0530

    Changed the discount method

--- Henry'sGrocery/src/com/ford/Henry.java
+++ Henry'sGrocery/src/com/ford/Henry.java
@@ -1,31 +1,15 @@
 package com.ford;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Henry {
-    private String name;
-    private int loyaltyPoints;
-
-    public Henry() {
-        this.name = "Henry";
-        this.loyaltyPoints = 0;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getLoyaltyPoints() {
-        return loyaltyPoints;
-    }
-
-    public void addLoyaltyPoints(int points) {
-        this.loyaltyPoints += points;
-    }
-
-    public double applyDiscount(double cost) {
-        if (this.loyaltyPoints >= 100) {
-            cost *= 0.9;
-            this.loyaltyPoints -= 100;
-        }
-        return cost;
-    }
+	   public double getDiscountRate(LocalDate purchaseDate) {
+	        // Determine if it's Tuesday
+	        if (purchaseDate.getDayOfWeek() == DayOfWeek.TUESDAY) {
+	            return 0.1; // 10% discount on Tuesdays
+	        } else {
+	            return 0.0; // No discount on other days
+	        }
+	    }
 }
